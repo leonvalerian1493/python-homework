ad43f4e8d8ec30570a8c83af1410c4c6a655ef65
leonvalerian1493 <maximillienjadin@yahoo.de>
2024-11-18 07:14:59 +0100

    Create DNA.py

---
+++ DNA.py
@@ -0,0 +1,14 @@
+```python
+s=""
+
+def count_occurences(dna):
+    
+        count_A = s.count("A")
+        count_G = s.count("G")
+        count_T = s.count("T")
+        count_C = s.count("C")    
+        return f"{count_A} {count_G} {count_T} {count_C}"
+
+result = count_occurences(s)
+print(result)
+```
