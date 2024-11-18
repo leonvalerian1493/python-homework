a51d7a119a8fad7717ea8c4bacc40edaa37d58fc
leonvalerian1493 <maximillienjadin@yahoo.de>
2024-11-18 07:25:06 +0100

    Create INI5.py

---
+++ INI5.py
@@ -0,0 +1,10 @@
+```python
+filepath = "./rosalind_data/INI5.txt"
+with open(filepath, 'r') as infile:
+    lines = infile.readlines()
+    for line in lines:
+        print(line.strip())
+
+for l in lines[1::2]:
+    print(l.strip())
+  ```
