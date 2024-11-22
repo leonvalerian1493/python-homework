39328160b568d05cf8d7504a4ac671fc3ea0a668
leonvalerian1493 <maximillienjadin@yahoo.de>
2024-11-22 09:38:37 +0100

    now reading all lines instead of just one

--- Rosalind/util.py
+++ Rosalind/util.py
@@ -4,7 +4,7 @@
 
 def read_input(filepath):
     with open(filepath, 'r') as infile:
-        lines = infile.readline()
+        lines = infile.readlines()
         stripped=[]
         for line in lines:
             stripped.append(line.strip())
